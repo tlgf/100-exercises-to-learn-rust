db20c450d2de8bdd24f51eabf658018d6345356d
tlgf <6648717+tlgf@users.noreply.github.com>
2024-12-07 20:59:47 +1100

    `for 1..n= {`

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,17 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0 {
+        return 1
+    }
+
+    let mut result = 1;
+
+    for i in 1..=n {
+        result *= i;
+    }
+
+    result
+
 }
 
 #[cfg(test)]
