bee8d2c0b09fc2d334a7852a300b063add57dfc2
tlgf <6648717+tlgf@users.noreply.github.com>
2024-12-13 23:39:39 +1100

    must think deeply about &mut vs mut

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,27 +11,60 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        Self::check_title(&title);
+        Self::check_desc(&description);
+        Self::check_status(&status);
 
         Ticket {
             title,
             description,
             status,
         }
+    }
+
+    fn check_title(title: &String) -> () {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+    }
+
+    fn check_desc(desc: &String) -> () {
+        if desc.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if desc.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+    }
+
+    fn check_status(status: &String) -> () {
+        let valid_status: Vec<String> = vec![
+            String::from("To-Do"),
+            String::from("In Progress"),
+            String::from("Done"),
+        ];
+
+        if !valid_status.contains(status) {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+    }
+
+    pub fn set_title(&mut self, new_title: String) -> () {
+        Self::check_title(&new_title);
+        self.title = new_title;
+    }
+
+    pub fn set_description(&mut self, new_desc: String) -> () {
+        Self::check_desc(&new_desc);
+        self.description = new_desc;
+    }
+
+    pub fn set_status(&mut self, new_status: String) -> () {
+        Self::check_status(&new_status);
+        self.status = new_status;
     }
 
     pub fn title(&self) -> &String {
