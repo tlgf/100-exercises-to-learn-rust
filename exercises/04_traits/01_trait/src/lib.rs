700f9a99996b83ddfdfd052e73955ff30368032a
tlgf <6648717+tlgf@users.noreply.github.com>
2024-12-20 23:42:02 +1100

    very neat

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -3,6 +3,26 @@
 //
 // Then implement the trait for `u32` and `i32`.
 
+trait IsEven {
+    fn is_even(self) -> bool;
+}
+
+impl IsEven for u32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0
+    }
+}
+
+impl IsEven for i32 {
+    fn is_even(self) -> bool {
+        if self < 0 {
+            return false
+        }
+        self % 2 == 0
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
